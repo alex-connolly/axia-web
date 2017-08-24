f60c8fade8921897e5a9f86e8675ada3f8e13703
communityalex <ndrpub@gmail.com>
2017-08-24 17:03:23 +1000

    changing bg colour

--- src/app/fixed/footer/footer.c.ts
+++ src/app/fixed/footer/footer.c.ts
@@ -10,5 +10,56 @@ import {Router} from "@angular/router";
 
 export class FooterComponent {
 
+    legalOptions = [
+        {
+            name: "Privacy Policy",
+            link: "privacy",
+        },
+        {
+            name: "Terms of Service",
+            link: "tos",
+        }
+    ]
+    developerOptions = [
+        {
+            name: "Documentation",
+            link: "docs",
+        },
+        {
+            name: "Support",
+            link: "support",
+        },
+        {
+            name: "Quickstart",
+            link: "quickstart"
+        },
+    ]
+    communityOptions = [
+        {
+            name: "Reddit",
+            link: "reddit.com/r/axia",
+            image: "",
+        },
+        {
+            name: "Github",
+            link: "github.com/axia",
+            image: "",
+        },
+        {
+            name: "Youtube",
+            link: "youtube.com/axia",
+            image: "",
+        },
+        {
+            name: "Facebook",
+            link: "facebook.com/axia",
+            image: "",
+        },
+        {
+            name: "Twitter",
+            link: "twitter.com/axiaprotocol",
+            image: "",
+        },
+    ]
 
 }
